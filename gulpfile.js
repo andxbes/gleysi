9386edd0466f78c7bfec1b7c7c011eb853e8930c
a.bestcennyi <andxbes@ya.ru>
2018-03-29 00:13:14 +0300

    hits

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,8 @@ gulp.task('css', function () {
 //            })
             )
             .pipe(cssBase64({
-                maxWeightResource: 4000,
+                baseDir: "../images",//путь до папки с изображениями которые следует обрабатывать  , относительно директории sass/css
+                maxWeightResource: 4000,// Размер в байтах , а не ширина 
                 extensionsAllowed: ['.gif', '.jpg', '.png','.svg']
             }))
             .pipe(autoprefixer(['last 2 versions', '> 1%', 'ie 8', 'ie 7', 'iOS >= 8', 'Safari >= 8']))
